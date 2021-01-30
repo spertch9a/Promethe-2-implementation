64334f1685e349df19216e9f052ec6dc623845f2
djawed <bekhouchadjawed@gmail.com>
2021-01-30 18:44:36 +0100

    Final commit

--- src/main/java/com/mycompany/promethe2/Algorithme.java
+++ src/main/java/com/mycompany/promethe2/Algorithme.java
@@ -7,7 +7,7 @@ package com.mycompany.promethe2;
 
 import static java.lang.Math.abs;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -31,6 +31,7 @@ public class Algorithme {
                     // prix
         Telephone telMaxPrix=Collections.max(matriceInitiale,Comparator.comparing(t -> t.getPrix()));
         Telephone telMinPrix=Collections.min(matriceInitiale,Comparator.comparing(t -> t.getPrix()));
+        
         double MaxPrix=telMaxPrix.getPrix();
         double MinPrix=telMinPrix.getPrix();
                     //stockage
@@ -62,8 +63,6 @@ public class Algorithme {
         for(int i=0;i<matriceInitiale.size();i++){
             MatriceNormalise.add(new Telephone(abs((MaxPrix-matriceInitiale.get(i).getPrix())/(MaxPrix-MinPrix)),abs((MinEspace-matriceInitiale.get(i).getEspace())/(MaxEspace-MinEspace)),
                     abs((MinCamera-matriceInitiale.get(i).getCamera())/(MaxCamera-MinCamera)),abs((MinLook-matriceInitiale.get(i).getLook())/(MaxLook-MinLook))));
-            
-            
         }
         
         return MatriceNormalise;
@@ -96,36 +95,161 @@ public class Algorithme {
     }
     
     
-    // etape 3 
-    
-    // je n'ai pas compris l'ecriture
-    
-    
-    // etape 4
-    
-    public ArrayList MultipleParPoids(ArrayList Array){
+    // etape 3 remplacer les valeurs négatives par 0 et garder les autres
+     public static  ArrayList calculPreference(ArrayList<Telephone> MatriceDiffercnes){
+        ArrayList<Telephone> MatricePreference= new ArrayList();
         
+         for(int i=0;i<MatriceDiffercnes.size();i++){
+           if(MatriceDiffercnes.get(i).getPrix()<0){
+            MatriceDiffercnes.get(i).setPrix(0);
+           }
+           if(MatriceDiffercnes.get(i).getEspace()<0){
+             MatriceDiffercnes.get(i).setEspace(0);
+           }
+           if(MatriceDiffercnes.get(i).getCamera()<0){
+            MatriceDiffercnes.get(i).setCamera(0);   
+           }
+           if(MatriceDiffercnes.get(i).getLook()<0){
+            MatriceDiffercnes.get(i).setLook(0); 
+           }
+          
+         }
+         MatricePreference=MatriceDiffercnes;
+        return MatricePreference;
+           }
+
+    // etape 4 Multiplier le tableau par les poids
+    
+    public static ArrayList MultipleParPoids(ArrayList<Telephone> MatricePreference){
+        ArrayList<Telephone> MatriceMultiplePoids= new ArrayList();
         
-        return  Array;
+        //Poids des critéres
+        double poidsprix=0.35;
+        double poidsespace=0.25;
+        double poidscamera=0.25;
+        double poidslook=0.15;
+        //Multiplier le tableau par les poids
+        for(int i=0;i<MatricePreference.size();i++){
+           MatriceMultiplePoids.add(new Telephone(MatricePreference.get(i).getPrix()*poidsprix,MatricePreference.get(i).getEspace()*poidsespace,
+                   MatricePreference.get(i).getCamera()*poidscamera,MatricePreference.get(i).getLook()*poidslook));
+           }
+                
+        return MatriceMultiplePoids;
     }
     
-    // etape5 + etape 6
+    // etape5 faire la somme du tableau
     
-    public ArrayList sommeLignes(ArrayList Array){
-        
+    public static ArrayList sommeLignes(ArrayList<Telephone> MatriceMultiplePoids){
+        ArrayList<Double> somme = new ArrayList();
         // faire la somme des lignes du tableau
-        
-        
-        
-        // créer un tableau a travers les sommes
-        
-        return Array;
+        for(int i=0;i<MatriceMultiplePoids.size();i++){
+           somme.add(MatriceMultiplePoids.get(i).getPrix()+MatriceMultiplePoids.get(i).getEspace()+
+                   MatriceMultiplePoids.get(i).getCamera()+MatriceMultiplePoids.get(i).getLook());
+           }
+    
+        return somme;
     }
     
+    //etape 6  créer un tableau a travers les sommes
+    public static double[][] MatriceSommes(ArrayList<Double> somme){
+        double[][] matrice = new double[5][5];
+
+        int i=0;
+
+            //boucles pour inserer dans la matrice
+            for (int y=0;y<4;y++){
+                for(int z=0;z<4;z++){
+                    if(y!=z){
+                        if(i< somme.size()){
+                            matrice[y][z]=somme.get(i);
+                            i++;
+                        }
+
+
+                    }else{
+                        matrice[y][z]=0;
+                    }
+
+
+                }
+            }
+
+
+
+
+
+
+
+
+        return matrice;
+    }
+
+
     
-    // etape 7
+    // etape 7 calcul du entering et leaving flow
+
+    public  static double[][] Flows(double[][] matriceSomme){
+        //flots sortants
+        for (int y=0;y<4;y++) {
+            double somme=0;
+            for (int z = 0; z < 4; z++) {
+                somme+=matriceSomme[y][z];
+
+            }
+            matriceSomme[y][4]=somme/3;
+        }
+
+        //flots entrants
+        for (int y=0;y<4;y++) {
+            double somme=0;
+            for (int z = 0; z < 4; z++) {
+                somme+=matriceSomme[z][y];
+
+            }
+            matriceSomme[4][y]=somme/3;
+        }
+
+        return matriceSomme;
+    }
     
-   // etape 8
+   // etape 8  difference entre leaving and entering flow
+
+    public static double[]  DifferenceFLows(double[][] matriceSomme){
+        double[] Difference= new double[4];
+
+
+            for (int i=0;i<4;i++){
+                Difference[i]=matriceSomme[i][4]- matriceSomme[4][i];
+
+                System.out.println("Telephone "+i+" "+Difference[i]);
+            }
+
+
+
+
+
+
+        return Difference;
+    }
+
+
+    //étape N° 9 trier les resultats
+
+    public static double[] sortingResults(double[] results){
+
+        Arrays.sort(results);
+        System.out.println("Le télephone le  plus adéquat est: ");
+        for (int i= results.length-1 ;i>=0;i--){
+            System.out.println("Télephone"+i+" valeur: "+results[i]);
+        }
+
+
+        return results;
+
+
+    }
+
+
     
     
     
--- src/main/java/com/mycompany/promethe2/Main.java
+++ src/main/java/com/mycompany/promethe2/Main.java
@@ -24,12 +24,34 @@ public class Main {
         tableau.add(new Telephone(200,16,8,3));
         tableau.add(new Telephone(300,32,16,4));
         tableau.add(new Telephone(275,32,8,2));
-        
+
+        //étape 1
         ArrayList<Telephone>MatriceNormalisee = Algorithme.normalisation(tableau);
+        //étape 2
         ArrayList<Telephone>MatriceDifferences= Algorithme.calculDifference(MatriceNormalisee);
- 
-        
+        //étape 3
+        ArrayList<Telephone>MatricePreference = Algorithme.calculPreference(MatriceDifferences);
+        //étape 4
+        ArrayList<Telephone>MatriceMultiplePoids = Algorithme.MultipleParPoids(MatricePreference);
+        //étape 5
+        ArrayList<Double>Somme=Algorithme.sommeLignes(MatriceMultiplePoids);
+
+
+        System.out.println(Somme);
+        //étape 6
+        double[][] MatriceSomme= Algorithme.MatriceSommes(Somme);
+        //étape 7
+        double[][] Matriceflows= Algorithme.Flows(MatriceSomme);
+        //étape 8
+        double[]  DifferenceFlows=Algorithme.DifferenceFLows(Matriceflows);
+        //étape 9
+        double[] SortedArray=Algorithme.sortingResults(DifferenceFlows);
+
         
     }
+
+
+
+
     
 }
